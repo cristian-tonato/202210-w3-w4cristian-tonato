294e728a126a482d494751713e4cdedf889d0811
Cristian <cristianotona@gmail.com>
2022-10-20 18:21:24 +0200

    create/files/src

---
+++ dist/components/component.js
@@ -0,0 +1,17 @@
+export class Component {
+    render(selector) {
+        if (document.querySelector(selector)) {
+            document.querySelector(selector).innerHTML = this.template;
+        }
+    }
+    addRender(selector) {
+        if (document.querySelector(selector)) {
+            document.querySelector(selector).innerHTML += this.template;
+        }
+    }
+    outRender(selector) {
+        if (document.querySelector(selector)) {
+            document.querySelector(selector).outerHTML = this.template;
+        }
+    }
+}
